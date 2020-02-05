1779da3c989901f24fa8359dda7f6c03bc004c4e
helloilya <ilya@fedotov.me>
2020-02-05 10:52:25 +0100

    Configure environment

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: [
+		'plugin:vue/essential',
+		'plugin:vue/recommended',
+	],
+	rules: {
+		'vue/v-on-style': ['error', 'shorthand'],
+		'vue/v-bind-style': ['error', 'shorthand'],
+		'vue/prop-name-casing': ['error', 'camelCase'],
+		'vue/mustache-interpolation-spacing': ['error', 'never'],
+		'vue/singleline-html-element-content-newline': 0,
+		'vue/html-closing-bracket-newline': 0,
+		'vue/max-attributes-per-line': 0,
+		'vue/html-indent': 0,
+	},
+	parserOptions: {
+		parser: 'babel-eslint',
+		sourceType: 'module',
+		ecmaVersion: 2018,
+	},
+	overrides: [
+		{
+			files: [
+				'src/**/*.spec.{j,t}s?(x)',
+			],
+			env: {
+				mocha: true,
+			},
+		},
+	],
+};
---
+++ README.md
@@ -0,0 +1,20 @@
+# Vue Weather App
+
+> Simple weather application based on Vue.js
+
+Please use the following [cli](https://cli.vuejs.org/config/) commands:
+
+```
+npm run serve
+npm run build
+npm run lint
+npm run test:unit
+```
+
+### Development
+
+Configuration options for eslint can be found [here](https://eslint.vuejs.org/rules/).
+
+## License
+
+It is open source software licensed as MIT.
