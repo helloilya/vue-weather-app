9998fb74e23f01292157698d56b5777dcc396a75
helloilya <ilya@fedotov.me>
2020-09-16 10:15:31 +0200

    Add home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,42 @@
 <template>
-	<div>
-		<h1>Home</h1>
+	<div class="container">
+		<WeatherCard :weather="weather" :unit-object="unitObject" />
+		<LocationSelect :location="location" @callback="updateWeather" />
 	</div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex';
+import LocationSelect from '@/components/LocationSelect';
+import WeatherCard from '@/components/WeatherCard';
+import { constants as settingStore } from '@/store/modules/setting';
+import { constants as weatherStore } from '@/store/modules/weather';
+
 export default {
 	name: 'Home',
 	metaInfo: {
-		title: 'Home',
+		title: 'Vue Weather App',
+	},
+	components: {
+		LocationSelect,
+		WeatherCard,
+	},
+	data: () => ({
+		location: '',
+	}),
+	computed: {
+		...mapGetters({
+			weather: weatherStore.getters.weather,
+			unitObject: settingStore.getters.unit,
+		}),
+	},
+	created() {
+		this.location = this.weather.name;
+	},
+	methods: {
+		...mapActions({
+			updateWeather: weatherStore.actions.update,
+		}),
 	},
 };
 </script>
